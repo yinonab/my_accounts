e76cacead04156f075d92ce8339a90c1d8244030
Yinonab <yinonabarjel@gmail.com>
2025-01-14 06:04:49 +0200

    add owner

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -85,7 +85,8 @@ export class ContactService {
             name: '',
             lastName: '',
             email: '',
-            phone: ''
+            phone: '',
+            owner: '',
         }
     }
 
